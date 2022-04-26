0e4ace3740b450997c7dd0fd53d4f8288716c46f
lucjan <lucjan.gackowski@gmail.com>
2022-04-26 19:58:01 +0200

    Bulk update
    Took 50 minutes

--- src/main.py
+++ src/main.py
@@ -1,15 +1,48 @@
+import relations
+from enum import Enum
+import random
+
+
+class State(Enum):
+    Susceptible = 1
+    Infected = 2
+    Recovered = 3
+
+
 class Agent:
     def __init__(self, age):
         self.age = age
-        self.state = "healthy"
+        self.state = State.Susceptible
 
     def change_state(self, new_state):
         self.state = new_state
 
-class Relationships:
-    def import_data(self):
+
+class Simulation:
+    def __init__(self,filename,constants):
+        self.relationships = relations.Relationships()
+        self.relationships.import_data(filename)
+        self.agents = self.create_agents(10_000)
+        self.constants = constants
+
+    def create_agents(self, n_agents):
+        result = []
+        for i in range(n_agents):
+            new_agent = Agent(random.randint(20,60))
+            result.append(new_agent)
+        return result
+
+    def add_agents(self, n_agents):
+        for i in range(n_agents):
+            new_agent = Agent(random.randint(20,60))
+            self.agents.append(new_agent)
+
+    def sim_step(self,n_step):
+        self.agents_spread_disease()
+        self.agents_recover()
+
+    def agents_recover(self):
         pass
-    def is_in_relation(self,n1,n2):
-        pass
-    def get_all_neighbours(self,n):
+
+    def agents_spread_disease(self):
         pass
---
+++ src/relations.py
@@ -0,0 +1,30 @@
+class Relationship:
+    def __init__(self, kto, z_kim, moc_relacji):
+        self.kto = kto
+        self.z_kim = z_kim
+        self.moc_relacji = moc_relacji
+
+
+class Relationships:
+    def __init__(self, n=10_000):
+        self.n = n
+        self.relationships = []
+
+    def import_data(self, file_name):
+        with open(file_name) as file:
+            dane = file.read().splitlines(False)
+            for line in dane:
+                kto, z_kim, moc_relacji = line.split(",")
+                new_relation = Relationship(kto, z_kim, moc_relacji)
+                self.relationships.append(new_relation)
+
+    def is_in_relation(self, n1, n2):
+        for element in self.relationships:
+            if element.kto == n1 and element.z_kim == n2:
+                return True
+        return False
+
+    def get_all_neighbours(self, n):
+        for element in self.relationships:
+            if element.kto == n:
+                yield element
